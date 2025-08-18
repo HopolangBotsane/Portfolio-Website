5b668dd4ece23b4ecdbe1940367ee9af915c7974
Thato Hopolang Botsane <HopolangBotsane@outlook.com>
2025-08-18 10:27:19 +0200

    Removes: react assets, readme.md
    Dependency: React Router Added
    
    Adds: Homepage, Projectspage & Contactpage

---
+++ portfolio/pages/Contactpage.jsx
@@ -0,0 +1,11 @@
+function contactpage() {
+
+    return(
+        <div className="contactpage">
+            <h1>Contact me</h1>
+        </div>
+    )
+    
+}
+
+export default Contactpage
---
+++ portfolio/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+function Homepage() {
+
+    return(
+        <div className="home-page">
+            <h1>Welcome Home</h1>
+        </div>
+    )
+    
+}
+
+export default Homepage
