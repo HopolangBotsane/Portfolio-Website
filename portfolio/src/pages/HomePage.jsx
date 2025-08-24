a5bfd121aaa2975b2ccae4401815b0b47cfd9289
Thato Hopolang Botsane <HopolangBotsane@outlook.com>
2025-08-24 21:31:13 +0200

    Adds styles: - index - Header - HomePage

--- portfolio/src/pages/HomePage.jsx
+++ portfolio/src/pages/HomePage.jsx
@@ -2,10 +2,11 @@ import Header from "../components/Header/Header"
 // import ProjectTile from "../components/ProjectTile"
 import Footer from "../components/Footer/Footer"
 import ContactSection from "../components/ContactSection/ContactForm"
+import styles from "./Pages.module.css"
 
 function HomePage() {
     return(
-        <div className="homepage">
+        <div className={styles.homepage}>
             <Header/>
             <div className="body">
                 <ContactSection/>
