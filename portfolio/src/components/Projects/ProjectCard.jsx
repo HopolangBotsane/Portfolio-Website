ed8066a98480faa3e1bed0c6d090b8cfd4f293f4
Thato Hopolang Botsane <HopolangBotsane@outlook.com>
2025-12-05 01:57:12 +0200

    Displayed CardData in ProjectCard component

--- portfolio/src/components/Projects/ProjectCard.jsx
+++ portfolio/src/components/Projects/ProjectCard.jsx
@@ -1,6 +1,29 @@
+import { projectCardData } from "./CardData"
+import { Link } from "react-router-dom"
+// 1. Import the CSS Module
+import styles from "./ProjectCard.module.css" 
+
 const ProjectCard = () => {
   return (
-    <div>ProjectCard</div>
+    <div>
+      {projectCardData.map((project) => {
+        const bgImage = project.strobeImage?.length > 0 
+          ? `url(${project.strobeImage[0]})` 
+          : 'none';
+
+        return (
+          <Link to={`/project/${project.id}`} key={project.id}>
+            <div 
+              className={styles.card} 
+              style={{ backgroundImage: bgImage }}
+            >
+              <h6>{project.projectCategory}</h6>
+              <h2>{project.projectName}</h2>
+            </div>
+          </Link>
+        )
+      })}
+    </div>
   )
 }
 
