5dc4c471502b8ae72a7d5ba5ab3ab5088f13391d
Thato Hopolang Botsane <HopolangBotsane@outlook.com>
2025-12-04 07:03:34 +0200

    Updates: - CardData with releavent information

--- portfolio/src/components/Projects/CardData.js
+++ portfolio/src/components/Projects/CardData.js
@@ -1,32 +1,153 @@
 export const projectCardData = [
-  {
-    id: 1,
-    title: "Title 1",
-    category: "Category 1",
-    slideImages: ["image1_1.jpg", "image2_1.jpg", "image3_1.jpg", "image4_1.jpg"],
-    pageLogo: [],
-    pageDescription: [],
-    fontSectionTitle: "Font",
-    fontSectionNumber: "03",
-    fontTitle: "This is a heading",
-    fontParagraph: "This is a paragraph that highlight how the selected font looks and how it contrasts against the rest",
-  },
-  {
-    id: 2,
-    title: "Title 2",
-    category: "Category 2",
-    slideImages: ["image1_2.jpg", "image2_2.jpg", "image3_2.jpg", "image4_2.jpg"]
-  },
-  {
-    id: 3,
-    title: "Title 3",
-    category: "Category 3",
-    slideImages: ["image1_3.jpg", "image2_3.jpg", "image3_3.jpg", "image4_3.jpg"]
-  },
-  {
-    id: 4,
-    title: "Title 4",
-    category: "Category 4",
-    slideImages: ["image1_4.jpg", "image2_4.jpg", "image3_4.jpg", "image4_4.jpg"]
-  }
+
+    {
+        id: 1,
+        projectName: "Project One",
+        projectCategory: "Design & Code",
+        strobeImage: [],
+        children: [
+            {
+                id: 101,
+                parentId: 2,
+                breakDownNumber: ".01",
+                breakDownName: "Introduction",
+                logoImages: [],
+            },
+            {
+                id: 102,
+                parentId: 1,
+                breakDownNumber: ".02",
+                breakDownName: "Colours",
+                colours: [],
+            },
+            {
+                id: 102,
+                parentId: 1,
+                breakDownNumber: ".03",
+                breakDownName: "Fonts",
+                fontTitle: "This is a heading with",
+                fontParagraph: "This is a paragraph that highlight how the selected font looks and how it contrasts against the rest",
+            }
+        ]
+    },
+    {
+        id: 2,
+        projectName: "Project Two",
+        projectCategory: "Design & Code",
+        strobeImage: [],
+        children: [
+            {
+                id: 201,
+                parentId: 2,
+                breakDownNumber: ".01",
+                breakDownName: "Introduction",
+                logoImages: [],
+            },
+            {
+                id: 102,
+                parentId: 2,
+                breakDownNumber: ".02",
+                breakDownName: "Colours",
+                colours: [],
+            },
+            {
+                id: 102,
+                parentId: 2,
+                breakDownNumber: ".03",
+                breakDownName: "Fonts",
+                fontTitle: "This is a heading with",
+                fontParagraph: "This is a paragraph that highlight how the selected font looks and how it contrasts against the rest",
+            }
+        ]
+    },
+    {
+        id: 3,
+        projectName: "Project Three",
+        projectCategory: "Design & Code",
+        strobeImage: [],
+        children: [
+            {
+                id: 301,
+                parentId: 3,
+                breakDownNumber: ".01",
+                breakDownName: "Introduction",
+                logoImages: [],
+            },
+            {
+                id: 302,
+                parentId: 3,
+                breakDownNumber: ".02",
+                breakDownName: "Colours",
+                colours: [],
+            },
+            {
+                id: 302,
+                parentId: 3,
+                breakDownNumber: ".03",
+                breakDownName: "Fonts",
+                fontTitle: "This is a heading with",
+                fontParagraph: "This is a paragraph that highlight how the selected font looks and how it contrasts against the rest",
+            }
+        ]
+    },    
+    {
+        id: 4,
+        projectName: "Project Four",
+        projectCategory: "Design & Code",
+        strobeImage: [],
+        children: [
+            {
+                id: 401,
+                parentId: 4,
+                breakDownNumber: ".01",
+                breakDownName: "Introduction",
+                logoImages: [],
+            },
+            {
+                id: 402,
+                parentId: 4,
+                breakDownNumber: ".02",
+                breakDownName: "Colours",
+                colours: [],
+            },
+            {
+                id: 402,
+                parentId: 4,
+                breakDownNumber: ".03",
+                breakDownName: "Fonts",
+                fontTitle: "This is a heading with",
+                fontParagraph: "This is a paragraph that highlight how the selected font looks and how it contrasts against the rest",
+            }
+        ]
+    },    
+    {
+        id: 5,
+        projectName: "Project Five",
+        projectCategory: "Design & Code",
+        strobeImage: [],
+        children: [
+            {
+                id: 501,
+                parentId: 5,
+                breakDownNumber: ".01",
+                breakDownName: "Introduction",
+                logoImages: [],
+            },
+            {
+                id: 502,
+                parentId: 5,
+                breakDownNumber: ".02",
+                breakDownName: "Colours",
+                colours: [],
+            },
+            {
+                id: 502,
+                parentId: 5,
+                breakDownNumber: ".03",
+                breakDownName: "Fonts",
+                fontTitle: "This is a heading with",
+                fontParagraph: "This is a paragraph that highlight how the selected font looks and how it contrasts against the rest",
+            }
+        ]
+    }
 ]
