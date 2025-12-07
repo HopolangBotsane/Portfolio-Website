a8a68d73ce86537f9e529f90654395ba6873b3d7
Thato Hopolang Botsane <HopolangBotsane@outlook.com>
2025-12-07 19:24:33 +0200

    Removes template code Removes vite package images Removes styling from styling pages
    Adds Homepage

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,7 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Homepage from './pages/Homepage.jsx';
+import './index.css';
+import App from './App.jsx';
+
+const router = [
+  {
+    path: "/",
+    element: <Homepage/>
+  }
+]
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
