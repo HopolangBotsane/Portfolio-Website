9fbc1b253b96c0f5426567b27341c2522564ae4c
Thato Hopolang Botsane <HopolangBotsane@outlook.com>
2025-12-07 23:36:53 +0200

    Moves HoverEmail & Email svg files to Header Completes Footer (no styles)

--- portfolio/src/components/Footer/Footer.jsx
+++ portfolio/src/components/Footer/Footer.jsx
@@ -1,6 +1,21 @@
+import Dribbble from "./Icons/Dribbble.svg"
+import Github from "./Icons/Github.svg"
+
 const Footer = () => {
+
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <div className="Footer">
+      <div className="left">
+        <p>&copy; {currentYear} Thato Hopolang Botsane</p>
+      </div>
+
+      <div className="right">
+        <img src={Dribbble} alt="Link to my Dribbble page" />
+        <img src={Github} alt="Link to my Github page" />
+      </div>
+    </div>
   )
 }
 
