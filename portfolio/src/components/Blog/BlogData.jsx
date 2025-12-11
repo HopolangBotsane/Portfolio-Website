065d63d79c664f14bcef5d64dd6495ecd567ed17
Thato Hopolang Botsane <HopolangBotsane@outlook.com>
2025-12-11 23:41:19 +0200

    Adds component Blog, css and data with content

---
+++ portfolio/src/components/Blog/BlogData.jsx
@@ -0,0 +1,10 @@
+export const blogPostData = [
+    {
+        id: 1,
+        img: "placeholder image",
+        imgAlt: "placeholder image description",
+        title: "placeholder title here",
+        paragraph: "placholder paragraph here",
+        tasksPerformed: ["Design", "Code"]
+    }
+]
