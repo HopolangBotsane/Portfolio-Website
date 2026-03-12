38b5e6d45087fd2d9f8bbb6b8750490b53068c3c
Thato Hopolang Botsane <HopolangBotsane@outlook.com>
2026-03-12 23:11:27 +0200

    Adds project images, project data and component

---
+++ portfolio/src/components/Project/Projects.jsx
@@ -0,0 +1,10 @@
+import projects from '../data/projects.json';
+import styles from "./Icons/Projects.module.css"
+
+export const Projects = () => {
+  return (
+    <section className={styles.Projects}>
+
+    </section>
+  )
+}
